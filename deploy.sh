3dccd437b623f2d55b592d7cd69c188a7bccd5df
lzjlxebr <lzjlxebr@lzjlxebrsmbp.local>
2018-11-25 12:49:42 +0800

    complete deploy logic

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 git pull
 
 # kill :8080
-nodepid=`netstat -anp | grep ":3000" | awk '{print $7}'|cut -d/ -f1` && kill -9 $nodepid
+nodepid=`netstat -anp | grep ":8080" | awk '{print $7}'|cut -d/ -f1` && kill -9 $nodepid
 echo $nodepid
 
 yarn notes:dev
